32f335508638a6e0ac3727e2e238fcb90bc9f3fc
Taras Kunch <taras.kunch@gmail.com>
2013-05-22 01:00:55 +0300

    Working example

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-var app = require('http').createServer(function(req, res) { res.writeHead(500); res.end('Nothing here'); })
-  , io = require('socket.io').listen(app);
+var fs = require('fs')
+  , server = require('./lib/server')(function(req, res) {
+      fs.readFile(__dirname + '/public/index.html',
+        function (err, data) {
+          if (err) {
+            res.writeHead(500);
+            return res.end('Error loading index.html');
+          }
 
-app.listen(5203);
+          res.writeHead(200);
+          res.end(data);
+        });
+    });
 
-io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-});
+server.listen(5203);
